6e745b88e6c150b4fb1e917456ff68370775608c
Stellanyambura <stellagitonga.com>
2023-02-09 09:41:51 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
+require "pry"
 class Dog
+    attr_reader:name
+    attr_writer:name
+    attr_reader:breed
+    attr_writer:breed
+    def name name
+        @name=name
+    end
 
-end
+    def name
+        @name
+    end
+
+    def breed breed
+        @breed=breed
+    end
+    def breed
+        @breed
+    end
+end 
+    bull=Dog.new
+    bill=Dog.new
+    
+    bull.name="Junnie"
+    bill.breed="bull dog"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    attr_reader:name
+    attr_writer:name
+    attr_reader:job
+    attr_writer:job
+    def name name
+        @name=name
+    end
 
+    def name
+        @name
+    end
+
+    def job job
+        @job=job
+    end
+    def job
+        @job
+    end
 end
